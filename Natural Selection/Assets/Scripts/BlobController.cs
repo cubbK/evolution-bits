e48f391b4856f1e7139b5207491a76e3ed6ee16a
cubbk <danpopa1998@gmail.com>
2019-08-13 01:13:40 +0300

    make blobs move

--- Natural Selection/Assets/Scripts/BlobController.cs
+++ Natural Selection/Assets/Scripts/BlobController.cs
@@ -6,9 +6,16 @@ public class BlobController : MonoBehaviour
 {
 
     public float movementSpeed = 1f;
+    private Vector2 direction = new Vector2(0, 1);
+    public Vector2 Direction
+    {
+        get => Vector2.ClampMagnitude(direction, 1);
+        set => direction = value;
+    }
     // IsometricCharacterRenderer isoRenderer;
 
     Rigidbody2D rbody;
+    int rotation = 0;
 
     private void Awake()
     {
@@ -21,11 +28,8 @@ public class BlobController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 currentPos = rbody.position;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-        inputVector = Vector2.ClampMagnitude(inputVector, 1);
-        Vector2 movement = inputVector * movementSpeed;
+        
+        Vector2 movement = Direction * movementSpeed;
         Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
         //isoRenderer.SetDirection(movement);
         rbody.MovePosition(newPos);
