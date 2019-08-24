2f53331f0f0ef75d072efa909c000be2e981dc1f
cubbk <danpopa1998@gmail.com>
2019-08-25 00:08:00 +0200

    add moving blob

--- Natural Selection/Assets/Scripts/BlobController.cs
+++ Natural Selection/Assets/Scripts/BlobController.cs
@@ -5,17 +5,72 @@ using UnityEngine;
 public class BlobController : MonoBehaviour
 {
 
-    public float movementSpeed = 1f;
-    private Vector2 direction = new Vector2(0, 1);
-    public Vector2 Direction
+    public float speed= 1f;
+    public float searchRadius = 1f;
+    public int reproductionRate = 10;
+
+    private Vector2 _direction = new Vector2(0.0f, 1.0f);
+    public Vector2 direction
     {
-        get => Vector2.ClampMagnitude(direction, 1);
-        set => direction = value;
+        get => Vector2.ClampMagnitude(_direction, 1);
+        set => _direction = value;
     }
     // IsometricCharacterRenderer isoRenderer;
 
     Rigidbody2D rbody;
     int rotation = 0;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        ChangeDirectionRandom();
+
+    }
+
+    public void ChangeDirectionRandom()
+    {
+        float randomX;
+        if(_direction.x > 0)
+        {
+            randomX = Random.Range(-1.0f, -0.1f);
+        } else
+        {
+            randomX = Random.Range(0.1f, 1.0f);
+        }
+        float randomY;
+        if (_direction.y > 0)
+        {
+            randomY = Random.Range(-1.0f, -0.1f);
+        }
+        else
+        {
+            randomY = Random.Range(0.1f, 1.0f);
+        }
+
+        int whichOneToAbsolute = Random.Range(0, 2);
+        if(whichOneToAbsolute == 0)
+        {
+            if(randomX < 0)
+            {
+                randomX = -1f;
+            } else
+            {
+                randomX = 1f;
+            }
+        } else
+        {
+            if (randomY < 0)
+            {
+                randomY = -1f;
+            }
+            else
+            {
+                randomY = 1f;
+            }
+        }
+
+        _direction = new Vector2(randomX, randomY);
+        Debug.Log(_direction);
+    }
 
     private void Awake()
     {
@@ -29,7 +84,7 @@ public class BlobController : MonoBehaviour
     {
         Vector2 currentPos = rbody.position;
         
-        Vector2 movement = Direction * movementSpeed;
+        Vector2 movement = direction * speed;
         Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
         //isoRenderer.SetDirection(movement);
         rbody.MovePosition(newPos);
---
+++ Natural Selection/Assets/Scripts/OnBlobFindingAnObstacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBlobFindingAnObstacle : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        string tag = other.gameObject.tag;
+        if (tag == "obstacle")
+        {
+            Transform parent = gameObject.transform.parent;
+            BlobController script = parent.GetComponent(typeof(BlobController)) as BlobController;
+            script.ChangeDirectionRandom();
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
